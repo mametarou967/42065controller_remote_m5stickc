f999b45c3f20123b1551cf971befbf4548cf7b6e
mametarou967 <mametarou967@gmail.com>
2023-04-22 17:31:00 +0900

    squareの実装

--- 42065_host_m5stickc/42065_host_m5stickc.ino
+++ 42065_host_m5stickc/42065_host_m5stickc.ino
@@ -37,6 +37,18 @@ int state_mode_square = STATE1_MODE_SQUARE;
 long mills_mode_square = 0;
 int count_mode_squre = 0;
 
+// TOF
+#define VL53L0X_REG_IDENTIFICATION_MODEL_ID         0xc0
+#define VL53L0X_REG_IDENTIFICATION_REVISION_ID      0xc2
+#define VL53L0X_REG_PRE_RANGE_CONFIG_VCSEL_PERIOD   0x50
+#define VL53L0X_REG_FINAL_RANGE_CONFIG_VCSEL_PERIOD 0x70
+#define VL53L0X_REG_SYSRANGE_START                  0x00
+#define VL53L0X_REG_RESULT_INTERRUPT_STATUS         0x13
+#define VL53L0X_REG_RESULT_RANGE_STATUS             0x14
+#define address                                     0x29  // I2C address
+
+byte gbuf[16];
+
 ArduinoQueue<int> queue(16); // 適当なサイズ
 
 /* Power Function */
@@ -45,28 +57,23 @@ int j=0;
 
 void stop()
 {
-  Serial.println("stop");
   pf.combo_pwm(PWM_FLT,PWM_FLT); 
 }
 
 void goForward(){
-  Serial.println("goForward");
   pf.combo_pwm(PWM_FWD4, PWM_REV4);
 }
 
 void goBackward(){
-  Serial.println("goBackward");
   pf.combo_pwm(PWM_REV4,PWM_FWD4);
 }
 
 void turnRight(){
-  Serial.println("turnRight");
-  pf.combo_pwm(PWM_FWD4, PWM_FWD4);
+  pf.combo_pwm(PWM_REV4, PWM_REV4);
 }
 
 void turnLeft(){
-  Serial.println("turnLeft");
-  pf.combo_pwm(PWM_REV4, PWM_REV4);
+  pf.combo_pwm(PWM_FWD4, PWM_FWD4);
 }
 
 // esp-now
@@ -156,7 +163,122 @@ void modeUpdate(int receiveCommand)
     DispScreen();
 }
 
+// ToF
+uint16_t bswap(byte b[]) {
+    // Big Endian unsigned short to little endian unsigned short
+    uint16_t val = ((b[0] << 8) & b[1]);
+    return val;
+}
+
+uint16_t makeuint16(int lsb, int msb) {
+    return ((msb & 0xFF) << 8) | (lsb & 0xFF);
+}
+
+void write_byte_data(byte data) {
+    Wire.beginTransmission(address);
+    Wire.write(data);
+    Wire.endTransmission();
+}
+
+void write_byte_data_at(byte reg, byte data) {
+    // write data word at address and register
+    Wire.beginTransmission(address);
+    Wire.write(reg);
+    Wire.write(data);
+    Wire.endTransmission();
+}
+
+void write_word_data_at(byte reg, uint16_t data) {
+    // write data word at address and register
+    byte b0 = (data & 0xFF);
+    byte b1 = ((data >> 8) && 0xFF);
+
+    Wire.beginTransmission(address);
+    Wire.write(reg);
+    Wire.write(b0);
+    Wire.write(b1);
+    Wire.endTransmission();
+}
+
+byte read_byte_data() {
+    Wire.requestFrom(address, 1);
+    while (Wire.available() < 1) delay(1);
+    byte b = Wire.read();
+    return b;
+}
+
+byte read_byte_data_at(byte reg) {
+    // write_byte_data((byte)0x00);
+    write_byte_data(reg);
+    Wire.requestFrom(address, 1);
+    while (Wire.available() < 1) delay(1);
+    byte b = Wire.read();
+    return b;
+}
+
+uint16_t read_word_data_at(byte reg) {
+    write_byte_data(reg);
+    Wire.requestFrom(address, 2);
+    while (Wire.available() < 2) delay(1);
+    gbuf[0] = Wire.read();
+    gbuf[1] = Wire.read();
+    return bswap(gbuf);
+}
+
+void read_block_data_at(byte reg, int sz) {
+    int i = 0;
+    write_byte_data(reg);
+    Wire.requestFrom(address, sz);
+    for (i = 0; i < sz; i++) {
+        while (Wire.available() < 1) delay(1);
+        gbuf[i] = Wire.read();
+    }
+}
+
+uint16_t VL53L0X_decode_vcsel_period(short vcsel_period_reg) {
+    // Converts the encoded VCSEL period register value into the real
+    // period in PLL clocks
+    uint16_t vcsel_period_pclks = (vcsel_period_reg + 1) << 1;
+    return vcsel_period_pclks;
+}
+
+int readToF()
+{
+  write_byte_data_at(VL53L0X_REG_SYSRANGE_START, 0x01);
+
+  byte val = 0;
+  int cnt  = 0;
+  while (cnt < 100) {  // 1 second waiting time max
+      delay(10);
+      val = read_byte_data_at(VL53L0X_REG_RESULT_RANGE_STATUS);
+      if (val & 0x01) break;
+      cnt++;
+  }
+  if (val & 0x01)
+  {
+    Serial.println("ready");
+  }
+  else
+  {
+    Serial.println("not ready");
+    return -1;
+  }
+
+  read_block_data_at(0x14, 12);
+  uint16_t acnt                  = makeuint16(gbuf[7], gbuf[6]);
+  uint16_t scnt                  = makeuint16(gbuf[9], gbuf[8]);
+  uint16_t dist                  = makeuint16(gbuf[11], gbuf[10]);
+  byte DeviceRangeStatusInternal = ((gbuf[0] & 0x78) >> 3);
+  int status = (int)DeviceRangeStatusInternal;
+
+  if(status != 11) return -1;
+
+  return dist;  
+}
+
+int read_tof = 0;
 void setup() {
+  Wire.begin();  // join i2c bus (address optional for master)
   M5.begin();
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setRotation(3);
@@ -220,15 +342,18 @@ void loop() {
       switch(state_mode_go_stop)
       {
         case STATE1_MODE_GO_STOP:
-          mills_mode_go_stop = millis();
+          // mills_mode_go_stop = millis();
           goForward();
           state_mode_go_stop++;
           break;
         case STATE2_MODE_GO_STOP:
-          if(millis() - mills_mode_go_stop > 2000)
+          read_tof = readToF(); 
+          if((read_tof != -1) && read_tof < 175)
           {
+            Serial.println("hello");
             stop();
             state_mode_go_stop++;
+            Serial.println("go stop -> stop");
           }
           else
           {
@@ -236,6 +361,7 @@ void loop() {
           }
           break;
         case STATE3_MODE_GO_STOP:
+          Serial.println("stop!!!");
           queue.enqueue(CMD_NOP);
           break;
       }
@@ -253,7 +379,7 @@ void loop() {
           state_mode_square++;
           break;
         case STATE3_MODE_SQUARE:
-          if(millis() - mills_mode_square > 2000)
+          if(millis() - mills_mode_square > 500)
           {
             mills_mode_square = millis();
             turnRight();
@@ -265,7 +391,7 @@ void loop() {
           }
           break;
         case STATE4_MODE_SQUARE:
-          if(millis() - mills_mode_square > 2000)
+          if(millis() - mills_mode_square > 575)
           {
             // 1セット終了
             count_mode_squre++;
